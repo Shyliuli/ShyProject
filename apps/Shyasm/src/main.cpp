d61ebdb49de3f9a9c961e36ee48005afeb810486
Shyliuli <1658347525@qq.com>
2025-09-21 20:20:11 +0800

    rm mock代码

--- apps/Shyasm/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "memory.hpp"
-int main(int argc, char** argv) {
-    auto memory = Memory::create();
-    memory.unwrap_or_else([](CoreError err) {
-        err.print();
-    });
-}
